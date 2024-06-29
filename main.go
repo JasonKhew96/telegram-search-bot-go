b4624449c3a7c25a53b843fb0c62d004e3b6e3fd
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2024-06-29 16:53:54 +0800

    fix: config file

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	importedFile := flag.String("import", "", "import tdesktop exported json file")
-	configFile := flag.String("config", "config.json", "config file")
+	configFile := flag.String("config", "config.yaml", "config file")
 	databaseFile := flag.String("database", "data.db", "database file")
 	flag.Parse()
 
